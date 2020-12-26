5e0c7aa3c887ef8e8d86e1054a4cafa18c2518ed
Jaewook Ahn <ajw4586@gmail.com>
2020-12-27 04:36:42 +0900

    Add utils module

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export const getEnv = () => process.env.NODE_ENV;
+export const isProduction = () => getEnv() == "production";
+
+export const log = {
+    verbose: (...args) => { if (!isProduction()) console.log(...args); },
+};

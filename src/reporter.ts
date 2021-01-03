96b7bba1bf8c0b7fea44006e7ec21655cdd50f66
Jaewook Ahn <ajw4586@gmail.com>
2021-01-04 03:51:30 +0900

    Refactor reporter class
    - extend EventEmitter
    - improve message send logic
    - remove unused constants
    - add error log function

--- src/reporter.ts
+++ src/reporter.ts
@@ -4,62 +4,35 @@
 import { Client, MessageEmbed, TextChannel } from "discord.js";
 import { format } from "date-fns";
 import { ko } from "date-fns/locale";
+import { EventEmitter } from "events";
 
 /**
  * Internal dependencies
  */
-import { StatusData, statusParser } from "./status_parser";
+import { StatusData } from "./status_parser";
 import { log } from "./utils";
-
-const REPORTING_CHANNEL_ID = "683591350767452190";
-const GUILD_ID = "662479388775809050";
-const GROUP_CODE = 7101;
 
 const getUrl = (groupCode: string) => `https://www.acmicpc.net/status?group_id=${groupCode}`;
 
-export class Reporter {
+export class Reporter extends EventEmitter {
 
     client: Client;
     token: string;
     guildId: string;
     channelId: string;
 
-
     constructor(token: string, guildId: string, channelId: string) {
+        super();
         this.token = token;
         this.guildId = guildId;
         this.channelId = channelId;
         this.client = new Client();
-        this.client.on("ready", async () => {
-            this.client.user.setActivity("채점 기록 확인");
-            const guild = await this.client.guilds.resolve(this.guildId).fetch();
-            const channel = guild.channels.resolve(channelId) as TextChannel;
-            const msg = await channel.messages.fetch({ limit: 1 });
-            const latestTimestamp = new Date (msg.map((m) => m.embeds[0])[0].timestamp).getTime();
-            let result = await statusParser.parse();
-            result = result.filter((row) => row.timestamp > latestTimestamp);
+        this.client.on("ready", this.handleClicentReady);
+    }
 
-            const queue = [];
-
-            result.reverse().forEach((row) => {
-                queue.push(this.generateReportMessage(row));
-            });
-
-            log.verbose("computed result\n", result);
-            if (!result.length) {
-                process.exit(0);
-            }
-
-            queue.forEach(async (message, i) => {
-                await channel.send(message);
-                log.verbose("message sent!");
-
-                if (i === queue.length - 1) {
-                    process.exit(0);
-                }
-            });
-        });
-
+    async handleClicentReady() {
+        this.emit("onLoginSuccess");
+        this.client.user.setActivity("채점 기록 확인");
     }
 
     login() {
@@ -69,10 +42,10 @@ export class Reporter {
         this.client.login(this.token);
     }
 
-    generateReportMessage(data: StatusData) {
+    generateReportMessage(data: StatusData, groupCode: string) {
         return new MessageEmbed()
             .setColor(0x0099ff)
-            .setURL(getUrl(this.))
+            .setURL(getUrl(getUrl(groupCode)))
             .addField("👤아이디 ", data.user_id)
             .addField("🔢 문제 번호 ", data.problem.num, true)
             .addField("📝 문제 이름 ", data.problem.name, true)
@@ -81,12 +54,26 @@ export class Reporter {
             .setTimestamp();
     }
 
-    setStatusData(data: StatusData) {
-        this.messages
-    }
+    async notify(statusData: StatusData[], groupCode: string) {
+        //  fetch target information
+        const guild = await this.client.guilds.resolve(this.guildId).fetch();
+        const channel = guild.channels.resolve(this.channelId) as TextChannel;
+        const msg = await channel.messages.fetch({ limit: 1 });
+        const latestTimestamp = new Date (msg.map((m) => m.embeds[0])[0].timestamp).getTime();
 
-    notify(groupCode?: string) {
+        statusData = statusData.filter((row) => row.timestamp > latestTimestamp);
+        if (!statusData.length) {
+            //  TODO throw error
+            log.error("No status data to send");
+            return;
+        }
+        log.verbose("status data\n", statusData);
 
+        const queue = statusData.reverse().map((row) => this.generateReportMessage(row, groupCode));
+        queue.forEach(async (message) => {
+            await channel.send(message);
+            log.verbose("message sent!");
+        });
     }
 
 }
--- src/utils.ts
+++ src/utils.ts
@@ -3,4 +3,5 @@ export const isProduction = () => getEnv() == "production";
 
 export const log = {
     verbose: (...args) => { if (!isProduction()) console.log(...args); },
+    error: (...args) => { if (!isProduction()) console.error(...args); },
 };
